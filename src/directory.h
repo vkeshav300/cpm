c7363c475812caba0dfd9e84f27620dba1514e6f
Keshav V <vkeshav300@gmail.com>
2023-07-10 09:06:16 -0700

    Add file pair commands

--- src/directory.h
+++ src/directory.h
@@ -1,3 +1,8 @@
+/*
+? You may be wondering why in other files, there is no checking if a file (or folder) already exists (or already doesn't exist) when creating or deleting.
+? This is because the methods to perform these actions checks file (or folder) existance by default.
+*/
+
 #pragma once
 
 #include <string>
@@ -6,15 +11,15 @@
 
 namespace directory
 {
-    // ? Converts file into string
+    // ? Convert file into string
     void slurp(std::ifstream &file, std::string *str);
 
-    // ? Splits string based off specific sequence of characters (delimiter)
+    // ? Split string based off specific sequence of characters (delimiter)
     std::vector<std::string> splitString(const std::string &input, const std::string &delimiter);
 
-    // ? Checks if a string has specific contents
+    // ? Check if a string has specific contents
     bool hasContents(std::string text, std::string contents);
-
+    
     bool hasFile(std::string dir, std::string filename);
 
     bool hasFolder(std::string dir, std::string foldername);

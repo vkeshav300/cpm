604e94b0165fd7a57ff555573ec3d2b39b842d28
Keshav Verma <vkeshav300@Keshavs-Air.MN-10>
2023-09-24 10:41:42 -0700

    Cleaned up comments

--- include/directory.h
+++ include/directory.h
@@ -1,8 +1,3 @@
-/*
-? You may be wondering why in other files, there is no checking if a file (or folder) already exists (or already doesn't exist) when creating or deleting.
-? This is because the methods to perform these actions checks file (or folder) existance by default.
-*/
-
 #pragma once
 
 #include <string>

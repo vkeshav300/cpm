41525bdd806995c5a38a6ab5f7caad220135002a
Keshav Verma <vkeshav300@Keshavs-MacBook-Air.local>
2023-09-30 18:08:08 -0700

    Code restructure

--- src/directory.cpp
+++ src/directory.cpp
@@ -3,9 +3,9 @@
  * @brief This file contains most directory-related functions that aren't commands.
  * @version 0.1
  * @date 2023-09-29
- * 
+ *
  * @copyright Copyright (c) 2023
- * 
+ *
  */
 #include "directory.h"
 #include "logger.h"
@@ -15,163 +15,166 @@
 #include <fstream>
 #include <filesystem>
 
-/**
- * @brief Reads file into string.
- * 
- * @param dir 
- * @param filename 
- * @return std::string 
- */
-std::string directory::slurp(std::string dir, std::string filename)
+namespace directory
 {
-    // * Checking if file exists
-    if (!hasFile(dir, filename))
+    /**
+     * @brief Reads file into string.
+     *
+     * @param dir
+     * @param filename
+     * @return std::string
+     */
+    std::string slurp(std::string dir, std::string filename)
     {
-        std::cerr << "\x1b[0;31m[error]: \x1b[0m\'"
-                  << filename
-                  << "\' does not exist at \'"
-                  << dir
-                  << "\'\n";
+        // * Checking if file exists
+        if (!hasFile(dir, filename))
+        {
+            std::cerr << "\x1b[0;31m[error]: \x1b[0m\'"
+                      << filename
+                      << "\' does not exist at \'"
+                      << dir
+                      << "\'\n";
 
-        logger::error("\'" + filename + "\' does not exist at \'" + dir + "\'");
-        
-        return NULL;
+            logger::error("\'" + filename + "\' does not exist at \'" + dir + "\'");
+
+            return NULL;
+        }
+
+        // * Opening the file
+        std::string filepath = dir + filename;
+
+        std::ifstream file;
+        file.open(filepath);
+
+        // * Reading contents of the file
+        std::string contents;
+        char ch;
+
+        while (file.get(ch))
+            contents += ch;
+
+        file.close();
+
+        return contents;
     }
 
-    // * Opening the file
-    std::string filepath = dir + filename;
-
-    std::ifstream file;
-    file.open(filepath);
-
-    // * Reading contents of the file
-    std::string contents;
-    char ch;
-    
-    while(file.get(ch))
-        contents += ch;
-
-    file.close();
-
-    return contents;
-}
-
-/**
- * @brief Checks if a file has specific contents.
- * 
- * @param text 
- * @param contents 
- * @return true 
- * @return false 
- */
-bool directory::hasContents(std::string text, std::string contents)
-{
-    if (text.find(contents) != std::string::npos)
-        return true;
-
-    return false;
-}
-
-/**
- * @brief Checks if directory has a file.
- * 
- * @param dir 
- * @param filename 
- * @return true 
- * @return false 
- */
-bool directory::hasFile(std::string dir, std::string filename)
-{
-    std::string filepath = dir + filename;
-
-    return std::filesystem::exists(filepath) && std::filesystem::is_regular_file(filepath);
-}
-
-/**
- * @brief Checks if directory has a folder.
- * 
- * @param dir 
- * @param foldername 
- * @return true 
- * @return false 
- */
-bool directory::hasFolder(std::string dir, std::string foldername)
-{
-    std::string folderpath = dir + foldername;
-
-    return std::filesystem::exists(folderpath) && std::filesystem::is_directory(folderpath);
-}
-
-/**
- * @brief Creates file in directory.
- * 
- * @param dir 
- * @param filename 
- */
-void directory::createFile(std::string dir, std::string filename)
-{
-    std::string filepath = dir + filename;
-
-    if (hasFile(dir, filename))
-        return;
-
-    std::ofstream file(filepath);
-    file.close();
-
-    if (!hasFile(dir, filename))
+    /**
+     * @brief Checks if a file has specific contents.
+     *
+     * @param text
+     * @param contents
+     * @return true
+     * @return false
+     */
+    bool hasContents(std::string text, std::string contents)
     {
-        logger::error("failed to create file " + filepath);
+        if (text.find(contents) != std::string::npos)
+            return true;
 
-        return;
+        return false;
     }
 
-    logger::success("created file " + filepath);
-}
-
-/**
- * @brief Creates folder in directory.
- * 
- * @param dir 
- * @param foldername 
- */
-void directory::createFolder(std::string dir, std::string foldername)
-{
-    std::string folderpath = dir + foldername;
-
-    if (hasFolder(dir, foldername))
-        return;
-
-    std::filesystem::create_directory(folderpath);
-
-    if (!hasFolder(dir, foldername))
+    /**
+     * @brief Checks if directory has a file.
+     *
+     * @param dir
+     * @param filename
+     * @return true
+     * @return false
+     */
+    bool hasFile(std::string dir, std::string filename)
     {
-        logger::error("failed to create folder " + folderpath);
+        std::string filepath = dir + filename;
 
-        return;
+        return std::filesystem::exists(filepath) && std::filesystem::is_regular_file(filepath);
     }
 
-    logger::success("created folder " + folderpath);
-}
-
-/**
- * @brief Deletes file in directory.
- * 
- * @param dir 
- * @param filename 
- */
-void directory::deleteFile(std::string dir, std::string filename)
-{
-    std::string filepath = dir + filename;
-
-    if (!hasFile(dir, filename))
-        return;
-
-    std::filesystem::remove(filepath);
-
-    if (hasFile(dir, filename))
+    /**
+     * @brief Checks if directory has a folder.
+     *
+     * @param dir
+     * @param foldername
+     * @return true
+     * @return false
+     */
+    bool hasFolder(std::string dir, std::string foldername)
     {
-        logger::error("failed to delete file " + filepath);
+        std::string folderpath = dir + foldername;
 
-        return;
+        return std::filesystem::exists(folderpath) && std::filesystem::is_directory(folderpath);
+    }
+
+    /**
+     * @brief Creates file in directory.
+     *
+     * @param dir
+     * @param filename
+     */
+    void createFile(std::string dir, std::string filename)
+    {
+        std::string filepath = dir + filename;
+
+        if (hasFile(dir, filename))
+            return;
+
+        std::ofstream file(filepath);
+        file.close();
+
+        if (!hasFile(dir, filename))
+        {
+            logger::error("failed to create file " + filepath);
+
+            return;
+        }
+
+        logger::success("created file " + filepath);
+    }
+
+    /**
+     * @brief Creates folder in directory.
+     *
+     * @param dir
+     * @param foldername
+     */
+    void createFolder(std::string dir, std::string foldername)
+    {
+        std::string folderpath = dir + foldername;
+
+        if (hasFolder(dir, foldername))
+            return;
+
+        std::filesystem::create_directory(folderpath);
+
+        if (!hasFolder(dir, foldername))
+        {
+            logger::error("failed to create folder " + folderpath);
+
+            return;
+        }
+
+        logger::success("created folder " + folderpath);
+    }
+
+    /**
+     * @brief Deletes file in directory.
+     *
+     * @param dir
+     * @param filename
+     */
+    void deleteFile(std::string dir, std::string filename)
+    {
+        std::string filepath = dir + filename;
+
+        if (!hasFile(dir, filename))
+            return;
+
+        std::filesystem::remove(filepath);
+
+        if (hasFile(dir, filename))
+        {
+            logger::error("failed to delete file " + filepath);
+
+            return;
+        }
     }
 }

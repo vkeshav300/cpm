12cb22c375c94381775b0d9fea558c1c6f0213e7
Keshav Verma <vkeshav300@gmail.com>
2025-02-26 12:51:01 -0800

    Add libcurl config variables

--- include/config.h
+++ include/config.h
@@ -9,8 +9,11 @@
  */
 #pragma once
 
+/* Update managing */
 constexpr char version_string[6] = "1.1.0";
 constexpr char repo_link[60] = "https://api.github.com/repos/vkeshav300/cpm/releases/latest";
+constexpr char updates_useragent[12] = "Mozilla/5.0";
 
+/* Init command */
 constexpr char c_default_standard[3] = "23";
 constexpr char cpp_default_standard[3] = "23";

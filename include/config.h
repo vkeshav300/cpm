43823cf187e124d10f874e6981bd05550cdaa04e
Keshav Verma <vkeshav300@gmail.com>
2025-02-25 18:27:05 -0800

    v1.1.0 staging

--- include/config.h
+++ include/config.h
@@ -2,6 +2,6 @@
 
 #include <string>
 
-const std::string version_string = "1.0.0";
+const std::string version_string = "1.1.0";
 const std::string c_default_standard = "23";
 const std::string cpp_default_standard = "23";

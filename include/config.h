2dccf18d406f48b3bbce285c5c979059f65060e5
Keshav Verma <vkeshav300@gmail.com>
2025-02-28 21:01:27 -0800

    Update config variables

--- include/config.h
+++ include/config.h
@@ -10,11 +10,14 @@
  */
 #pragma once
 
+#include <cstdint>
+
 /* Update managing */
 constexpr char version_string[6] = "1.2.0";
 constexpr char repo_link[60] =
     "https://api.github.com/repos/vkeshav300/cpm/releases/latest";
 constexpr char updates_useragent[12] = "Mozilla/5.0";
+constexpr uint16_t default_usc_freq = 5;
 
 /* Init command */
 constexpr char c_default_standard[3] = "23";
